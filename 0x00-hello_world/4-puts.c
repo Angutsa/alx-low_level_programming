8d4b3bc0aafed39407cd9e477471b666d3665889
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2022-12-13 20:28:33 +0300

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,12 +2,15 @@
 
 /**
   * main - Prints a sentence to the standard output
-  * @void - Does not need any inputs
-  * Return - 0 if successful
+  * Description: Function prints out the string value set in the string
+  * sentence. This function is not interactive with the user.
+  * @void: Does not need any inputs
+  * Return: 0 if successful
   */
 int main(void)
 {
 	char *sentence = "Programming is like building a multilingual puzzle";
+
 	puts(sentence);
-	return 0;
+	return (0);
 }
