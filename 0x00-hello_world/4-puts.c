2485b8a35308ae7c3c97da412c2639245d1f43da
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2022-12-13 20:19:51 +0300

    Add 4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <std.io.h>
+
+/**
+  * main - Prints a sentence to the standard output
+  * @void - Does not need any inputs
+  * Return - 0 if successful
+  */
+int main(void)
+{
+	char *sentence = "Programming is like building a multilingual puzzle";
+	puts(sentence);
+	return 0;
+}
