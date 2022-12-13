55ca98299fe36f6c03929cc9129e243374bb0ffd
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2022-12-13 20:55:37 +0300

    Add program to print the size of different types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - prints out size of different variable types
+  * @void: Takes no input
+  * Description: The function prints out the sizes of a char, int, long int,
+  * long long int and float. This is based on the system it is currently
+  * running on.
+  * Return: Returns 0 if successful
+  */
+int main(void)
+{
+	printf("Size of a char: %lu byte(s)\n", (unsigned long) sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long) sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", (unsigned long) sizeof(long int));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long) sizeof(long long int));
+	printf("Size of a float: %lu byte(s)\n", (unsigned long) sizeof(float));
+	return (0);
+}
