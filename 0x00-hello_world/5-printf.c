71fc7fe3a010e388581283424f30bc9ce8470bac
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2022-12-13 20:36:07 +0300

    Add script to print line using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - Prints out a sentence
+  * @void: Does not take arguments
+  * Description: This function outputs a sentence to the standard output
+  * using the printf function only.
+  * Return: 0 if successful
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
