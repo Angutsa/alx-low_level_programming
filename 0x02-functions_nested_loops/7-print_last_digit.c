1d4c17011fed5efcaa5ae5d22e54aafd5c86235f
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2022-12-15 00:14:56 +0300

    Add program to print the last digit of a number

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,9 +8,10 @@
   */
 int print_last_digit(int m)
 {
-	if (m < 0)
-		m *= -1;
+	int last_digit = m % 10;
+	if (last_digit < 0)
+		last_digit = last_digit * -1;
 
-	 _putchar('0' + (m % 10));
-	return (m % 10);
+	_putchar('0' + last_digit);
+	return (last_digit);
 }
