df5a68e66e42a66d4f373d4f224f7d7a287e204a
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2022-12-15 02:09:09 +0300

    Add program to add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * add - adds two integers
+  * @n: - integer 1
+  * @m: - integer 2
+  * Decsription: Adds two integers and returns the result
+  * Return: int - Sum of the two integers
+  */
+int add(int n, int m)
+{
+	return (n + m);
+}
