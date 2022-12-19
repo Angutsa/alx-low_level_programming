cc3dd53b9d4eb3a6cf0751cb9a9e111de9c4c2f2
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2022-12-19 23:07:14 +0300

    Add program to print last half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+void puts_half(char *str)
+{
+	int ltr = 0;
+	int half;
+
+	for (;;)
+	{
+		if (*(str + ltr) == '\0')
+			break;
+
+		ltr++;
+	}
+
+	if ((ltr % 2) != 0)
+		ltr -= 1;
+
+	half = ltr / 2;
+
+	for (;;)
+	{
+		if (*(str + half) == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+
+		_putchar(*(str + half));
+		half++;
+	}
+}
