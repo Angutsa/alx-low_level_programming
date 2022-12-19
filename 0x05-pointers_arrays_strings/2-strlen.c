fa0baf2194d5e79776434cb45623691944a3e7b2
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2022-12-19 20:37:53 +0300

    Add program that returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+/**
+  * _strlen - returns the length of a string
+  * @s: pointer to the string
+  * Return: no return
+  */
+int _strlen(char *s)
+{
+	int ltr = 0;
+	int count = 0;
+
+	for (;;)
+	{
+		if (*(s + ltr) == '\0')
+		{
+			return (count);
+		}
+		ltr++;
+		count++;
+	}
+}
