28a5e9b2ab5e2d22171bcf76b83a5d049f963cb0
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-01-05 23:27:30 +0300

    Add a function that gets the square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+int _sqrt(int low, int high, int x);
+
+/**
+  * _sqrt_recursion - returns the natural square root of a number
+  * @n: number to square root
+  * Return: int - natural square root of the number
+  */
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+
+	return (_sqrt(0, n, n));
+}
+
+/**
+  * _sqrt - calculates the sqaure root of a number using binary search
+  * @high: the upper limit of the square root
+  * @low: the lower limit of the square root
+  * @x: sqaure number
+  * Return: sqaure root of the number
+  */
+int _sqrt(int low, int high, int x)
+{
+	int y = (low + high) / 2;
+
+	if ((high - low) == 1)
+		return (-1);
+
+	if ((y * y) == x)
+		return (y);
+
+	if ((y * y) > x)
+	{
+		return (_sqrt(low, y, x));
+	}
+	else
+	{
+		return (_sqrt(y, high, x));
+	}
+}
