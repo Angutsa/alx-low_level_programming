07802f410fc7fa2a7308be7b6bb4ee25cd2182ef
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-01-16 21:54:00 +0300

    Fix the name of a folder

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+  * main - prints all possible combinations of two two-digit numbers
+  * @void: no input
+  * Description: Prints all possible combinations of two two-digits
+  * without repeating the digits (i.e 00 01 and 01 00 are considered
+  * the same combination of two two-digits). The digits printed
+  * are not the same and are printed in ascending order.
+  * Return: 0 on success
+  */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 1000)
+	{
+		if ((i % 100) != (i / 100)
+	}
+	putchar('\n');
+
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,2 +1,9 @@
-This folder contains C programs written when learning more about
-argc and argv.
+This folder contains C programs created when learning about argc
+and argv. The programs are:
+
+0-whatsmyname.c
+1-args.c
+100-change.c
+2-args.c
+3-mul.c
+4-add.c
