29fde0f4258563b48b0ae19e83923399b0e7809f
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-01-26 22:21:41 +0300

    Fix function

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,18 +13,16 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list ptr;
 	int i;
 
-	if (separator == NULL)
-		separator = "";
-
 	va_start(ptr, n);
 
 	for (i = 0; i < (int) n; i++)
 	{
 		printf("%i", va_arg(ptr, int));
 
-		if (i != (int) (n - 1))
+		if (separator != NULL && i != (int) (n - 1))
 			printf("%s ", separator);
 	}
 
 	printf("\n");
+	va_end(ptr);
 }
