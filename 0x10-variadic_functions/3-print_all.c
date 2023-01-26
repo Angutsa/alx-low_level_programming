e7c8944da24021c6a804cc28152483537bcd212b
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-01-26 23:21:00 +0300

    Add function to print all characters

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,51 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+  * print_all - prints anything
+  * @format: list of types of arguments passed to the function
+  */
+void print_all(const char * const format, ...)
+{
+	va_list(ptr);
+	int i = 0;
+	char *s;
+	char x;
+
+	va_start(ptr, format);
+	while (format[i] != '\0')
+	{
+		x = format[i];
+		switch (x)
+		{
+		case 'c':
+			printf("%c", va_arg(ptr, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(ptr, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(ptr, double));
+			break;
+		case 's':
+			s = va_arg(ptr, char *);
+			if (s == NULL)
+				printf("(nil)");
+			else
+				printf("%s", s);
+			break;
+		default:
+			i++;
+			continue;
+		}
+		while (format[i + 1] != '\0')
+		{
+			printf(", ");
+			break;
+		}
+		i++;
+	}
+	printf("\n");
+	va_end(ptr);
+}
