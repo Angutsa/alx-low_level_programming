80041f79067b8c1ca80f2af130a96ab4fa4ef9d7
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-01-26 21:19:56 +0300

    Add a function to initialise structure

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "dog.h"
+
+/**
+  * init_dog - function that initialises struct dog variable
+  * @d: pointer to the struct
+  * @name: name of the dog
+  * @age: age of the dog
+  * @owner: owner of the dog
+  */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
