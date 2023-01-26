87e89f43bd8c4c203ff40a8e41cf9acc6cd229b8
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-01-26 16:56:22 +0300

    Fix program

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,55 +10,46 @@
   */
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, ltrs = 1;
+	int i = 0, j, ltrs = 1;
 	char *str = NULL;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
 
-	if (s1 != NULL)
+	if (s2 == NULL)
+		s2 = "";
+	
+	for (j = 0; *(s1 + j) != '\0'; j++)
 	{
-		for (i = 0; *(s1 + i) != '\0'; i++)
-		{
-			ltrs++;
-		}
+		ltrs++;
 	}
 
-	if (s2 != NULL)
+	for (j = 0; *(s2 + j) != '\0'; j++)
 	{
-		for (i = 0; *(s2 + i) != '\0'; i++)
-		{
-			ltrs++;
-		}
+		ltrs++;
 	}
-
+	
 	str = malloc(sizeof(char) * ltrs);
 	if (str == NULL)
 		return (NULL);
 
-	if (s1 != NULL)
-	{		
-		for (i = 0, j = 0; i < ltrs; i++, j++)
-		{
-			if (*(s1 + i) == '\0')
-				break;
-			
-			*(str + i) = *(s1 + j);
-		}
+	j = 0;
+	while (s1[j] && i < ltrs)
+	{
+		str[i] = s1[j];
+		j++;
+		i++;
 	}
 
-	if (s2 == NULL)
+	j = 0;
+	while (s2[j] && i < ltrs)
 	{
-		*(str + ltrs) = '\0';
-		return (str);
-	}
-	
-	for (j = 0; i < ltrs; i++, j++)
-	{
-		*(str + i) = *(s2 + j);
+		str[i] = s2[j];
+		j++;
+		i++;
 	}
 
-	*(str + ltrs) = '\0';
+	str[ltrs] = '\0';
 
 	return (str);
 }
