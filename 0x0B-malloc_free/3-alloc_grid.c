eee12d174e459223d2caefe52e05fa4963785bd7
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-01-26 17:28:35 +0300

    Add a program to return a pointer to a 2D array

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+  * alloc_grid - returns a pointer to a 2 dim. array of integers
+  * @width: width of matrix
+  * @height: height of matrix
+  * Return: pointer to 2 dimensional array
+  */
+int **alloc_grid(int width, int height)
+{
+	int i, j, num = width * height;
+	int **rows;
+	int *x;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	rows = malloc(sizeof(int *) * height);
+	if (rows == NULL)
+		return (NULL);
+
+	x = malloc(sizeof(int) * num);
+	if (x == NULL)
+		return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		rows[i] = x + (i * height);
+
+		for (j = 0; j < width; j++)
+		{
+			*(rows[i] + j) = 0;
+		}
+	}
+
+	return (rows);
+}
