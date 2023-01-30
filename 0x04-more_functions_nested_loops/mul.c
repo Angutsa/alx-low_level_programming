3c900d0b7781838a420eb94f35efc9a6a8c4ef8b
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-01-30 21:09:40 +0300

    Add a function to multiply teo integers

---
+++ 0x04-more_functions_nested_loops/mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * mul - multiplies two integers
+  * @t: number 1
+  * @v: number 2
+  * Return: Returns the product of number 1 and 2
+  */
+int mul(int t, int v)
+{
+	return (t * v);
+}
