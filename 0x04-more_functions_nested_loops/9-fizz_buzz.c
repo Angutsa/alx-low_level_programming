51d2a0f8228df5448624aeed248a33a48a2496f7
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-01-30 21:44:01 +0300

    Add a FizzBuzz program

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the numbers 1-100 with FizzBuzz replacements
+ * Description: Prints the numbers 1-100. Program prints Fizz for
+ * multiples of 3, Buzz for multiples of 5 and FizzBuzz for multiples
+ * of both 3 and 5.
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 1; b <= 100; b++)
+	{
+		if ((b % 3) == 0)
+			printf("Fizz");
+
+		if ((b % 5) == 0)
+			printf("Buzz");
+
+		if ((b % 3) != 0 && (b % 5) != 0)
+			printf("%i", b);
+
+		printf(" ");
+	}
+
+	printf("\n");
+	return (0);
+}
+
