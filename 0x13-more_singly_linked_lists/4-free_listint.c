10660eebc03c3687a4f7adc8f8710b713150bf42
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-02-05 02:40:37 +0300

    Add a function to free a listint_t list

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+  * free_listint - frees a listint_t
+  * @head: pointer to head node
+  */
+void free_listint(listint_t *head)
+{
+	if (head->next != NULL)
+		free_listint(head->next);
+
+	free(head);
+}
