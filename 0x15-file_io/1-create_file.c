70a9e4d2e71f43ed6e302627247909c303c19ebc
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-02-12 22:34:10 +0300

    Fix function to create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,14 +20,15 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL || *filename == '\0')
 		return (-1);
 
-	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC,	S_IRUSR | S_IWUSR);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC,
+			S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
 
 	if (text_content == NULL)
 	{
 		close(fd);
-		return (0);
+		return (1);
 	}
 
 	while (text_content[i] != '\0')
@@ -35,7 +36,7 @@ int create_file(const char *filename, char *text_content)
 		i++;
 	}
 
-	wr = write(fd, text_content, (i + 1));
+	wr = write(fd, text_content, i);
 	if (wr == -1)
 	{
 		close(fd);
