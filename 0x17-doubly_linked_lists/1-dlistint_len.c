17e3f551192033f05267d01abe3c13bb8e375ba9
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-03-16 21:16:57 +0300

    Add solution for task 0x17-01

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -3,14 +3,14 @@
 #include <stdio.h>
 #include "lists.h"
 
-int print_elements(dlistint_t *h);
+int count_elements(dlistint_t *h);
 
 /**
-  * print_dlistint - prints the elements in a dlistint_t list
+  * dlistint_len - returns the number of elements in a dlistint_t list
   * @h: pointer to any node in the list
   * Return: the number of nodes
   */
-size_t print_dlistint(const dlistint_t *h)
+size_t dlistint_len(const dlistint_t *h)
 {
 	dlistint_t *head = NULL;
 	int count = 0;
@@ -21,20 +21,19 @@ size_t print_dlistint(const dlistint_t *h)
 		head = head->prev;
 	}
 
-	count = print_elements(head);
+	count = count_elements(head);
 	return (count);
 }
 
 /**
-  * print_elements - counts the nodes in the list from the head
+  * count_elements - counts the nodes in the list from the head
   * @h: pointer to the head node of the list
   * Return: the number of nodes
   */
-int print_elements(dlistint_t *h)
+int count_elements(dlistint_t *h)
 {
 	if (h == NULL)
 		return (0);
 
-	printf("%d\n", h->n);
-	return (1 + print_elements(h->next));
+	return (1 + count_elements(h->next));
 }
