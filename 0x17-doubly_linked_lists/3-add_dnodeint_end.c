4e2ac36c95e7ae8c7fd7fc4fe198e6b041e7bce3
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2023-03-16 21:51:17 +0300

    Add solution for task 0x17-03

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,39 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+  * add_dnodeint_end - adds a new node at the end of a dlistint_t list
+  * @head: list head node
+  * @n: element to add to linked list
+  * Return: new element or NULL if it fails
+  */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node = NULL, *h = NULL;
+
+	new_node = malloc(sizeof(dlistint_t *));
+	if (new_node == NULL)
+		return (NULL);
+
+	if (*head == NULL)
+	{
+		*head = new_node;
+		new_node->prev = NULL;
+	}
+	else
+	{
+		h = *head;
+		while (h->next != NULL)
+		{
+			h = h->next;
+		}
+		new_node->prev = h;
+		h->next = new_node;
+	}
+
+	new_node->n = n;
+	new_node->next = NULL;
+	return (new_node);
+}
