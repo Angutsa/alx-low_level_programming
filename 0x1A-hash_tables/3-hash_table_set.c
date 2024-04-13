7fd3e5ddda4f0144d737975b01bc82996e234379
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2024-04-13 17:13:49 +0300

    Fix code

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -4,62 +4,75 @@
 #include <string.h>
 #include "hash_tables.h"
 
+int update_nodes(hash_node_t *node, char *key, char *value);
+
 /**
  * hash_table_set - adds an element to the hash table
  * @ht: hash table to add the element to
  * @key: the element's key.
  * @value: the value associated with the key
+ * Return: 0 on failure; 1 on success
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	const unsigned char *value_dup = (const unsigned char *) value;
-	unsigned long int index, size = ht->size;
-	hash_node_t *element = NULL;
+	char *value_dup;
+	unsigned long int index;
+	hash_node_t *element;
 	hash_node_t **array = ht->array;
 
-	/* Check input */
 	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
 
-	/* Ensure key is not empty */
 	if (strlen(key) == 0)
 		return (0);
 
-	/* Generate index */
-	index = key_index((const unsigned char *) key, size);
+	value_dup = strdup(value);
+	if (value_dup == NULL)
+		return (0);
 
-	/* if index empty, add node */
-	if (array[index] == NULL)
+	index = key_index((const unsigned char *) key, ht->size);
+
+	/* update relavent nodes. if not, add node at the beginning */
+	if (update_nodes(array[index], (char *) key, value_dup)  == 0)
 	{
-		/* Create node */
 		element = malloc(sizeof(hash_node_t));
 		if (element == NULL)
 			return (0);
 
 		element->key = (char *) key;
-		element->value = (char *) value_dup;
-		element->next = NULL;
-	}
-	else
-	{
-		/* Check if key needs updating */
-		if (strcmp(array[index]->key, (char *) key) == 0)
-		{
-			free(array[index]->value);
-	 		array[index]->value = (char *)value_dup;
-			return (1);
-		}
-
-		/* Otherwise add node to the very beginning */
-		element = malloc(sizeof(hash_node_t));
-		if (element == NULL)
-			return (0);
-
-		element->key = (char *) key;
-		element->value = (char *) value_dup;
+		element->value = value_dup;
 		element->next = array[index];
 		array[index] = element;
 	}
 
 	return (1);
+}
+
+/**
+ * update_nodes - updates the nodes in a linked list whose key has been
+ * provided
+ * @key: key
+ * @value: value to be associated with the key
+ * @node: node to check
+ * @updated: indicates if any linked node was updated
+ * Return: 1 if any node has been updated, 0 if not
+ */
+int update_nodes(hash_node_t *node, char *key, char *value)
+{
+	int updated = 0;
+
+	if (node == NULL)
+		return (updated);
+
+	if (strcmp(node->key, key) == 0)
+	{
+		free(node->value);
+		node->value = value;
+		updated = 1;
+	}
+
+	if (update_nodes(node->next, key, value) == 1)
+		updated = 1;
+
+	return (updated);
 }
