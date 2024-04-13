f44ae2e7298e6e6830b011ab201bf5cae392a320
Angutsa-UBUNTU-Line <64151460+Angutsa@users.noreply.github.com>
2024-04-13 19:23:20 +0300

    A bloody semi-colon

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+i#include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include "hash_tables.h"
@@ -17,7 +17,7 @@ void hash_table_print(const hash_table_t *ht)
 	if (ht == NULL)
 		return;
 
-	size = (int) ht->size
+	size = (int) ht->size;
 
 	/* Print all key-value pairs */
 	printf("{");
